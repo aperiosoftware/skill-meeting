5cec33ab55520b0ef489452e5858e8eb9fd0b958
Stuart Mumford <stuart@cadair.com>
2020-07-11 15:48:45 +0100

    Initial working version

--- README.md
+++ README.md
@@ -11,10 +11,12 @@ It has or will have the the following basic features:
 
 It might in the future grow any of the following:
 
+* [ ] Reorder the agenda
 * [ ] Schedule meetings.
-* [ ] Suggest you schedule a meeting when the list of agenda items gets too long.
+* [ ] Suggest you schedule a meeting when:
+    - [ ] The list of agenda items gets too long.
+    - [ ] After a certain amount of time since the last meeting.
+    - [ ] When a certain % of the action items are complete.
 * [ ] Record notes from the meeting.
 * [ ] Track action items from the meeting.
 * [ ] Remind people about their action items on timer, or in the run up to the next meeting.
-
-
--- skill_meeting/skill.py
+++ skill_meeting/skill.py
@@ -1,18 +1,31 @@
 import logging
+from typing import List, Dict
 
-import jinja2
+import markdown
 
+from opsdroid import events
 from opsdroid.skill import Skill
 from opsdroid.matchers import match_event, match_parse
 
 log = logging.getLogger("skill_meeting.skill")
 
 
+ITEM_TEMPLATE = """\
+* {title}
+
+    {body}
+"""
+
+AGENDA_TEMPLATE = """\
+# Agenda
+{items}
+"""
+
 class Meeting(Skill):
     """
     Manage meetings with opsdroid.
     """
-    async def extend_agenda(self, title, body):
+    async def extend_agenda(self, title: str, body: str) -> None:
         """
         Extend the existing agenda in memory.
         """
@@ -21,23 +34,29 @@ class Meeting(Skill):
         agenda.append({"title": title, "body": body})
         await self.opsdroid.memory.put("agenda", agenda)
 
+    async def generate_agenda_md(self, agenda: List[Dict[str, str]]) -> str:
+        items = [ITEM_TEMPLATE.format(**item) for item in agenda]
+        return AGENDA_TEMPLATE.format(items="\n".join(items))
+
     @match_parse('!agenda "{title}" "{body}"')
     @match_parse('!agenda {title}')
-    async def add_to_agenda(self, message):
+    async def add_to_agenda(self, message: events.Message) -> None:
         title = message.parse_result["title"]
         body = ""
         if "body" in message.parse_result:
             body = message.parse_result["body"]
         await self.extend_agenda(title, body)
 
+        await message.respond("Added to the agenda.")
+
     @match_parse('!agenda')
-    async def show_agenda(self, message):
+    async def show_agenda(self, message: events.Message) -> None:
         agenda = await self.opsdroid.memory.get("agenda")
         if agenda:
-            await message.respond(str(agenda))
+            await message.respond(markdown.markdown(await self.generate_agenda_md(agenda)))
         else:
             await message.respond("There is no agenda yet.")
 
     @match_parse('!agenda clear')
-    async def clear_agenda(self, message):
+    async def clear_agenda(self, message: events.Message) -> None:
         self.opsdroid.memory.put("agenda", None)
